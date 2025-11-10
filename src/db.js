2c0e84217ce083119f9b6b81c1a2e9b7727766fd
Gabriel Bello <125406298+gbellooo@users.noreply.github.com>
2025-11-10 21:10:04 +0000

    banco

--- src/db.js
+++ src/db.js
@@ -5,11 +5,12 @@ dotenv.config();
 export const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'projeto.cluster-custom-ctuciooo6jwt.us-east-1.rds.amazonaws.com',
     port: process.env.DB_PORT || 5432,
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'postgres',
-    database: process.env.DB_NAME || 'sensors_db'
+    database: process.env.DB_NAME || 'sensors_db',
+    ssl: { rejectUnauthorized: false } // necessário para conexão segura com o RDS
   },
   pool: { min: 0, max: 10 }
 });
